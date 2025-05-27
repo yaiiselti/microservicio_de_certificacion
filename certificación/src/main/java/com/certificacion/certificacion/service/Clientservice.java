7a3173e24b3cf9ecca7924eeceb847ded1db08b1
yaii <yaiiselti258@gmail.com>
2025-05-27 13:36:14 -0400

    url

--- certificación/src/main/java/com/certificacion/certificacion/service/Clientservice.java
+++ certificación/src/main/java/com/certificacion/certificacion/service/Clientservice.java
@@ -1,16 +1,17 @@
 package com.certificacion.certificacion.service;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-import java.util.Map;
 
 @Service
 public class Clientservice {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
-    private static final String USUARIO_SERVICE_BASE_URL = "http://8081/api/usuarios/";
-    private static final String CLASE_SERVICE_BASE_URL = "http://8082/api/clases/";
+    private static final String USUARIO_SERVICE_BASE_URL = "http://192.168.56.1:8081/api/usuarios";
+    private static final String CLASE_SERVICE_BASE_URL = "http://192.168.56.1:8080/api/clases";
 
     public Map<String, String> getUsuarioDetails(String usuarioId) {
         String url = USUARIO_SERVICE_BASE_URL + usuarioId;
